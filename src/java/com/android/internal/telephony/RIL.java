6f7a5b1f0c0d874bb1ac33109910709932bf0e30
klozz <klozz707@gmail.com>
2018-04-22 20:12:57 -0400

    RIL: fix typo introduced in https://github.com/TheXPerienceProject/android_frameworks_opt_telephony/commit/92a9da1fe6a0f7b6de33b34417f658ff4460325a
    Signed-off-by: klozz <klozz707@gmail.com>

--- src/java/com/android/internal/telephony/RIL.java
+++ src/java/com/android/internal/telephony/RIL.java
@@ -5190,7 +5190,7 @@ public final class RIL extends BaseCommands implements CommandsInterface {
     static SignalStrength convertHalSignalStrength(
             android.hardware.radio.V1_0.SignalStrength signalStrength) {
 
-	    String hardware = android.os.Syste,Properties.get("ro.hardware", "");
+	    String hardware = android.os.SystemProperties.get("ro.hardware", "");
         if(hardware.contains("hi3660") || hardware.contains("hi6250") || hardware.contains("hi3670") || hardware.contains("kirin970"))
             return convertHalSignalStrengthHuawei(signalStrength);
 
