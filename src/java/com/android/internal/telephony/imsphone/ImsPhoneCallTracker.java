53cc77329cceecd276ede9128374b7a64f8786dd
Quallenauge <Hamsi2k@freenet.de>
2022-08-03 21:16:11 +0100

    telephony: Disable handling audio direction changes on IMS calls.
    I6f84e2b90e572c0973dcf04ce1e0c8e5e3524467 (AOSP) introduces
    handling of changes of the audio direction between major call state
    changes.
    
    This breaks ringback tone when establishing IMS calls
    on other devices, so make it configurable.
    
    Change-Id: I29331b7e8ea8f048f4567518b2173c6d7e503a6c
    Signed-off-by: klozz <carlosj@klozz.dev>

--- src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
+++ src/java/com/android/internal/telephony/imsphone/ImsPhoneCallTracker.java
@@ -3435,10 +3435,15 @@ public class ImsPhoneCallTracker extends CallTracker implements ImsPullCall {
         conn.updateMediaCapabilities(imsCall);
         if (ignoreState) {
             conn.updateAddressDisplay(imsCall);
-            // Some devices will change the audio direction between major call state changes, so we
-            // need to check whether to start or stop ringback
-            conn.maybeChangeRingbackState();
 
+            boolean handleAudioDirectionChangesBetweenCallStateChanges =
+                SystemProperties.getBoolean(
+                    "ro.telephony.handle_audio_direction_changes_between_call_state_changes", true);
+            if (handleAudioDirectionChangesBetweenCallStateChanges) {
+                // Some devices will change the audio direction between major call state change,
+                // so we need to check whether to start or stop ringback
+                conn.maybeChangeRingbackState();
+            }
             maybeSetVideoCallProvider(conn, imsCall);
             // Update IMS call status if the call attributes are changed - i.e. call network type.
             mImsCallInfoTracker.updateImsCallStatus(conn);
